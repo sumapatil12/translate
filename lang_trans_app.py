a43e31bbf90dda20453c152104ffe221f7fa07f7
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2023-11-24 04:39:05 +0530

    Update lang_trans_app.py

--- lang_trans_app.py
+++ lang_trans_app.py
@@ -44,6 +44,6 @@ if submit_button:
 
        # Display the results
         #translate = output[0]['translation_text'].replace('<n>', " ")
-        translate = output[0]['translation_text']
+        translate = output[0]['translation_text']['inputs']
         st.write(f"Your Input Text: **{text_inp}**")
         st.write(f"**{translate}**")
